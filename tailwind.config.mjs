1f5960389f401e1e5927a1b5aaf40da68e7e72de
Kenneth Quintero <kennethgqv@gmail.com>
2024-12-24 19:29:42 -0700

    wip

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				background: "#030404",
-				foreground: "#DCBA71",
-				"secondary-foreground": "#54472B",
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#0B0B0B",
+        foreground: "#C0C0C0",
+        primary: "#DCBA71",
+        secondary: "#54472B",
+      },
+    },
+  },
+  plugins: [],
+};
