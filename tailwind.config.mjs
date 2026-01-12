9683d16e4ca12f1b0d0ed8efee6825c8768fb80d
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2026-01-11 23:05:09 -0700

    THIS IS THE ONE BROTHA

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,27 +4,26 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["PPSupplyMono", "system-ui", "sans-serif"]
+        sans: ["PPSupplyMono", "system-ui", "sans-serif"],
       },
       colors: {
-        background: "#13160E",
-        foreground: "#DBE6B6",
-        muted: "hsla(74, 100%, 10%, 1)",
-        primary: "#DBE6B6",
-        secondary: "#475569",
-        blumine: {
-          50: "#f1f9fa",
-          100: "#dbecf2",
-          200: "#bcdae5",
-          300: "#8dc0d3",
-          400: "#579cb9",
-          500: "#3c809e",
-          600: "#346986",
-          700: "#335c75",
-          800: "#2e495c",
-          900: "#2a3f4f",
-          950: "#182734",
-        },
+        // first option
+        // background: "#13160E",
+        // foreground: "#DBE6B6",
+        // primary: "#DBE6B6",
+        // muted: "hsla(74, 100%, 10%, 1)",
+        // secondary: "#475569",
+
+        // Second option
+        background: "#151515",
+        foreground: "#C0BEAF",
+        primary: "#C0BEAF",
+        muted: "#2c2c2c",
+
+        // Third option
+        // background: "#FFFFFF",
+        // foreground: "#333333",
+        // primary: "#333333",
       },
     },
   },
